ab3260f0c39ee328b812b7911cbfd04fa409d6c2
iadeiza <iadeiza@gmail.com>
2022-09-11 15:42:48 -0700

    my sixth commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entrypoint
+ *
+ * Return: Always return 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char i;
+	char e;
+	char q;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'e' & 'q')
+			{
+			putchar(i);
+			}
+	}
+	putchar('\n');
+	return (0);
+}
