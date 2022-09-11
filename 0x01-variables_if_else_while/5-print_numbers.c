63e672d28e226bcde1b9af338c03531e3718d96d
iadeiza <iadeiza@gmail.com>
2022-09-11 16:32:15 -0700

    my 4th commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: print integers
+ *
+ * return: Always return 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
