a36c2ce8273df96defd08d10df5529a6da40dc11
iadeiza <iadeiza@gmail.com>
2022-09-14 13:10:18 -0700

    my header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,9 +6,9 @@
 
 int _putchar(char c);                                                                                                           
 
-print_alphabet(void);                                                                                                      
+void print_alphabet(void);                                                                                                      
 
-print_alphabet_x10(void);                                                                                                  
+void print_alphabet_x10(void);                                                                                                  
 
 int _islower(int c);                                                                                                            
 
