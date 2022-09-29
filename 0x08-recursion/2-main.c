7ab5ec510ddfe7d536e3f055a636affd1ab09a71
iadeiza <iadeiza@gmail.com>
2022-09-29 14:04:43 -0700

    my recursion commit

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - checks the code
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Coleman");
+	printf("%d\n", n);
+	return (0);
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-my complie repo
