c9e6eb592ac834a7789ea4a3775f8fedb5fe9819
iadeiza <iadeiza@gmail.com>
2022-10-04 11:40:34 -0700

    my static library project

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - checks the code
+ * @argc: variable that holds the count of argument
+ * @argv: array containing strings of argument
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
