367998157fe0bf222b0d0691b8f524ce5a671657
iadeiza <iadeiza@gmail.com>
2022-10-05 14:35:57 -0700

    my malloc commt

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H_
 #define MAIN_H_
-
+#include <stdlib.h>
+#include <stdio.h>
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
